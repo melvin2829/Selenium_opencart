d3f80f2a8d2fe56cf22f5baffdc7212813577d62
unknown <basutkarmelvin@gmail.com>
2025-10-22 10:33:22 +0530

    Fix stale element and updated pages.py

--- pages.py
+++ pages.py
@@ -2,81 +2,87 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.common.action_chains import ActionChains
+import time
 
 class BasePage:
-    def __init__(self, driver, timeout=10):
+    def __init__(self, driver, timeout=15):
         self.driver = driver
         self.wait = WebDriverWait(driver, timeout)
 
     def find(self, by, locator):
         return self.wait.until(EC.presence_of_element_located((by, locator)))
 
-    def find_all(self, by, locator):
-        return self.wait.until(EC.presence_of_all_elements_located((by, locator)))
+    def find_visible(self, by, locator):
+        return self.wait.until(EC.visibility_of_element_located((by, locator)))
 
     def click(self, by, locator):
-        self.find(by, locator).click()
+        el = self.find_visible(by, locator)
+        el.click()
 
-    def type(self, by, locator, text):
-        el = self.find(by, locator)
+    def type(self, by, locator, text, retries=3):
+        """Safe send_keys with retry in case of StaleElementReferenceException."""
+        for attempt in range(retries):
+            try:
+                el = self.find_visible(by, locator)
+                el.clear()
+                el.send_keys(text)
+                return
+            except StaleElementReferenceException:
+                time.sleep(1)
+        # last try
+        el = self.find_visible(by, locator)
         el.clear()
         el.send_keys(text)
 
     def js_click(self, by, locator):
-        el = self.find(by, locator)
+        el = self.find_visible(by, locator)
         self.driver.execute_script("arguments[0].click();", el)
 
     def hover(self, el):
         ActionChains(self.driver).move_to_element(el).perform()
 
+
 class HomePage(BasePage):
-    SEARCH = (By.NAME, "search")
-    SEARCH_BTN = (By.CSS_SELECTOR, "div#search button")
+    SEARCH = (By.NAME, "search")       # locator for search box
+    SEARCH_BTN = (By.CSS_SELECTOR, "button.btn.btn-default.btn-lg")  # search button
 
     def search(self, term):
+        """Search for a product using the search bar."""
         self.type(*self.SEARCH, term)
         self.click(*self.SEARCH_BTN)
 
+
 class LoginPage(BasePage):
-    MY_ACCOUNT = (By.XPATH, "//a[@title='My Account']")
-    LOGIN = (By.LINK_TEXT, "Login")
+    LOGIN_MENU = (By.LINK_TEXT, "My Account")
+    LOGIN_LINK = (By.LINK_TEXT, "Login")
     EMAIL = (By.ID, "input-email")
     PASSWORD = (By.ID, "input-password")
-    LOGIN_BTN = (By.CSS_SELECTOR, "input[type='submit'][value='Login']")
+    LOGIN_BTN = (By.CSS_SELECTOR, "input[value='Login']")
 
     def open_login(self):
-        self.click(*self.MY_ACCOUNT)
-        self.click(*self.LOGIN)
+        self.click(*self.LOGIN_MENU)
+        self.click(*self.LOGIN_LINK)
 
-    def login(self, email, pwd):
-        self.type(*self.EMAIL, email)
-        self.type(*self.PASSWORD, pwd)
+    def login(self, username, password):
+        self.type(*self.EMAIL, username)
+        self.type(*self.PASSWORD, password)
         self.click(*self.LOGIN_BTN)
+        time.sleep(2)  # wait for page to stabilize after login
+
 
 class ProductPage(BasePage):
-    FIRST_PRODUCT = (By.CSS_SELECTOR, "div.product-layout div.caption a")
-    ADD_TO_CART = (By.ID, "button-cart")  # depends on page
-
-    def open_first(self):
-        self.click(*self.FIRST_PRODUCT)
+    ADD_TO_CART = (By.ID, "button-cart")
 
     def add_to_cart(self):
         self.click(*self.ADD_TO_CART)
 
+
 class CartPage(BasePage):
-    CART_BUTTON = (By.ID, "cart")           # quick view
-    VIEW_CART = (By.LINK_TEXT, "View Cart") # open cart page
-    TABLE_ROWS = (By.CSS_SELECTOR, "table.table tbody tr")
+    CART_BUTTON = (By.ID, "cart")
+    VIEW_CART = (By.LINK_TEXT, "View Cart")
 
     def open_cart(self):
         self.click(*self.CART_BUTTON)
-        # sometimes need to click view cart
-        try:
-            self.click(*self.VIEW_CART)
-        except:
-            pass
-
-    def has_product(self, name):
-        rows = self.find_all(*self.TABLE_ROWS)
-        return any(name.lower() in r.text.lower() for r in rows)
+        self.click(*self.VIEW_CART)
